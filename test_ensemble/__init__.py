a60eadc36e3e207079707f4a152956442383d9f7
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-05-11 19:51:56 +0900

    Update

--- test_ensemble/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 09:40, 11/05/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieunguyen5991                                                  %
-#-------------------------------------------------------------------------------------------------------%
-
